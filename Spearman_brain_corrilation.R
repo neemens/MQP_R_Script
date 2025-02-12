19c0a155b81af2601ff598a25e00908a698ef3a0
neemens <158086403+neemens@users.noreply.github.com>
2025-02-12 13:13:20 -0500

    Add files via upload

---
+++ Spearman_brain_corrilation.R
@@ -0,0 +1,50 @@
+install.packages("data.table")
+install.packages("tidyverse")
+install.packages("writexl")
+install.packages("readxl")
+
+library(readxl)
+library(tidyverse);
+library(data.table);
+library(writexl);
+
+
+# Read the data sets
+
+average_brain_data <- read_excel("TPM_Averages_by_Brain_Region.xlsx", sheet = 1)
+
+# Extract the expression values for the  RFX isoform
+rfx_isoform_1 <- average_brain_data[69296, ] %>%
+  select(-ensgid, -enstid)
+
+rfx_isoform_1 <- as.numeric(unlist(rfx_isoform_1))# Convert to numeric values
+
+# Prepare to store correlation results
+isoform_1_results <- data.frame(Isoform = character(),
+                                P_Value = numeric(),
+                                Correlation = numeric(),
+                                stringsAsFactors = FALSE)
+
+
+for (i in 1:nrow(average_brain_data)) {# Loop through each brain isoform and compute Spearman correlation
+  # Extract the expression values for the current brain isoform
+  brain_isoform <- average_brain_data[i, ] %>%
+    select(-ensgid, -enstid)
+  
+  brain_isoform <- as.numeric(unlist(brain_isoform)) # Convert to numeric values
+  
+  
+  cor_test <- cor.test(rfx_isoform_1, brain_isoform, method = "spearman")# Perform Spearman correlation test
+  
+  # Store the results 
+  isoform_1_results[i, "Isoform"] <- as.character(average_brain_data$enstid[i])
+  isoform_1_results[i, "P_Value"] <- cor_test$p.value
+  isoform_1_results[i, "Correlation"] <- cor_test$estimate
+}
+
+
+# Write the Excel file 
+write_xlsx(isoform_1_results, "spearman_corrilation_analysis_RFX1.xlsx")
+
+
+
