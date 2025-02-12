57a96e5d10602b393dcad233262384ba64c90efc
neemens <158086403+neemens@users.noreply.github.com>
2025-02-12 13:04:16 -0500

    spearman_testis_script.R

--- spearman_testis_script.R
+++ spearman_testis_script.R
@@ -16,13 +16,13 @@ library(minerva)
 testis_data <- read_excel("Testis_expression_data.xlsx", sheet = 2)
 
 # Extract the expression values for the  RFX isoform
-rfx_isoform_3 <- testis_data[1, ] %>%
+rfx_isoform_1 <- testis_data[1, ] %>%
   select(-ensgid, -enstid)
 
-rfx_isoform_3 <- as.numeric(unlist(rfx_isoform_3))# Convert to numeric values
+rfx_isoform_1 <- as.numeric(unlist(rfx_isoform_1))# Convert to numeric values
 
 # Prepare to store correlation results
-isoform_3_results <- data.frame(Gene = character(),
+isoform_1_results <- data.frame(Gene = character(),
                                 Isoform = character(),
                                 P_Value = numeric(),
                         
@@ -37,16 +37,16 @@ for (i in 1:nrow(testis_data)) {# Loop through each brain isoform and compute Sp
 testis_isoform <- as.numeric(unlist(testis_isoform)) # Convert to numeric values
   
   
-cor_test <- cor.test(rfx_isoform_3, testis_isoform, method = "spearman")
+cor_test <- cor.test(rfx_isoform_1, testis_isoform, method = "spearman")
 
 # Store the results
-isoform_3_results[i, "Gene"] <- as.character(testis_data$ensgid[i])
-isoform_3_results[i, "Isoform"] <- as.character(testis_data$enstid[i])
-isoform_3_results[i, "P_Value"] <- cor_test$p.value
-isoform_3_results[i, "spearman"] <- cor_test$estimate
+isoform_1_results[i, "Gene"] <- as.character(testis_data$ensgid[i])
+isoform_1_results[i, "Isoform"] <- as.character(testis_data$enstid[i])
+isoform_1_results[i, "P_Value"] <- cor_test$p.value
+isoform_1_results[i, "spearman"] <- cor_test$estimate
 }
 
 # Write the Excel file
-write_xlsx(isoform_3_results, "spearman_testis_RFX1.xlsx")
+write_xlsx(isoform_1_results, "spearman_testis_RFX1.xlsx")
 
 
