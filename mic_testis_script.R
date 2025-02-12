f95029e17e26fd24199a0ffde6c323ad19b651fc
neemens <158086403+neemens@users.noreply.github.com>
2025-02-12 13:02:00 -0500

    mic_testis_script.R

--- mic_testis_script.R
+++ mic_testis_script.R
@@ -16,13 +16,13 @@ library(minerva)
 testis_data <- read_excel("Testis_expression_data.xlsx", sheet = 2)# Read the data sets
 
 # Extract the expression values for the  RFX isoform
-rfx_isoform_3 <- testis_data[1, ] %>%
+rfx_isoform_1 <- testis_data[1, ] %>%
   select(-ensgid, -enstid)
 
-rfx_isoform_3 <- as.numeric(unlist(rfx_isoform_3))# Convert to numeric values
+rfx_isoform_1 <- as.numeric(unlist(rfx_isoform_1))# Convert to numeric values
 
 # Prepare to store correlation results
-isoform_3_results <- data.frame(Gene = character(),
+isoform_1_results <- data.frame(Gene = character(),
                                 Isoform = character(),
                                 stringsAsFactors = FALSE)
 
@@ -35,14 +35,14 @@ for (i in 1:nrow(testis_data)) {# Loop through each brain isoform and compute co
   testis_isoform <- as.numeric(unlist(testis_isoform)) # Convert to numeric values
   
   
-  mic_value <- mine(rfx_isoform_3, testis_isoform)$MIC # perform MIC corrilation
+  mic_value <- mine(rfx_isoform_1, testis_isoform)$MIC # perform MIC corrilation
   
   # Store the results
-  isoform_3_results[i, "Gene"] <- as.character(testis_data$ensgid[i])
-  isoform_3_results[i, "Isoform"] <- as.character(testis_data$enstid[i])
-  isoform_3_results[i, "MIC"] <- mic_value
+  isoform_1_results[i, "Gene"] <- as.character(testis_data$ensgid[i])
+  isoform_1_results[i, "Isoform"] <- as.character(testis_data$enstid[i])
+  isoform_1_results[i, "MIC"] <- mic_value
 }
 
 # Write the Excel file
-write_xlsx(isoform_3_results, "mic_testis_RFX1.xlsx")
+write_xlsx(isoform_1_results, "mic_testis_RFX1.xlsx")
 
